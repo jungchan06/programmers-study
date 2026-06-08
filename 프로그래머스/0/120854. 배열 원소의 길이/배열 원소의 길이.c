6489905923d532e82d4dd72761e2b9661289502d
이정찬 <bangge0677@gmail.com>
2026-06-08 18:26:43 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int* solution(const char* strlist[], size_t strlist_len) {
+    int* answer = (int*)malloc(sizeof(int) * strlist_len);
+
+    for (int i = 0; i < strlist_len; i++) 
+    {
+        answer[i] = strlen(strlist[i]);
+    }
+
+    return answer;
+}
