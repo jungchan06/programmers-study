b506b74bfa093d20061fc2eebd807a8ab2154c33
이정찬 <bangge0677@gmail.com>
2026-06-23 22:43:21 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 9.96 ms, Memory: 5.07 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.c
@@ -0,0 +1,15 @@
+long long solution(int a, int b) {
+    long long answer = 0;
+
+    if (a > b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    for (int i = a; i <= b; i++) {
+        answer += i;
+    }
+
+    return answer;
+}
