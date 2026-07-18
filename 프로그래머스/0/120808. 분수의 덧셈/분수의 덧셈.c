f59546073c18c74cc9ccec1c1ef1c601d30a276d
이정찬 <bangge0677@gmail.com>
2026-07-18 15:15:17 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 5.14 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.c
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// 최대공약수 함수
 int gcd(int a, int b)
 {
     while (b != 0)
@@ -17,14 +16,14 @@ int* solution(int numer1, int denom1, int numer2, int denom2)
 {
     int* answer = (int*)malloc(sizeof(int) * 2);
 
-    // 분수 더하기
+   
     int numer = numer1 * denom2 + numer2 * denom1;
     int denom = denom1 * denom2;
 
-    // 최대공약수
+   
     int g = gcd(numer, denom);
 
-    // 기약분수
+
     answer[0] = numer / g;
     answer[1] = denom / g;
 
