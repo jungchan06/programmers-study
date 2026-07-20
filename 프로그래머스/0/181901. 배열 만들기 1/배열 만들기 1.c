bc58c00fd8cf6829ec62a279f17a05a8c6c3762b
이정찬 <bangge0677@gmail.com>
2026-07-21 01:42:58 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.49 ms, Memory: 5.01 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int n, int k) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(sizeof(int) * (n/k));
+    
+    int x = k;
+    
+    for(int i = 0; i < (n/k); i++)
+    {
+        answer[i] = x;
+        x += k;
+    }
+    return answer;
+}
